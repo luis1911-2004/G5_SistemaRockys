befb9d119abbacd958e6c53dd7cc0f7718b19c92
luis1911-2004 <luisnamaypalacios2004@gmail.com>
2025-12-04 19:06:28 -0500

    Menu Principal

--- G5_SistemaRockys/Form1.Designer.cs
+++ G5_SistemaRockys/Form1.Designer.cs
@@ -28,84 +28,210 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btn_Mproveedor = new System.Windows.Forms.Button();
-            this.btn_Mtipoinsumos = new System.Windows.Forms.Button();
-            this.btn_Mmotivoreclamo = new System.Windows.Forms.Button();
-            this.btn_Mformapago = new System.Windows.Forms.Button();
-            this.btn_Mestadopreparacion = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.btnregnoconformidadinsumos = new System.Windows.Forms.Button();
+            this.btnrecepcioncomanda = new System.Windows.Forms.Button();
+            this.btnmonitoreopedidos = new System.Windows.Forms.Button();
+            this.btnfallamantenimiento = new System.Windows.Forms.Button();
+            this.btn_requirimientos = new System.Windows.Forms.Button();
+            this.btn_realizanotaingresoinsumo = new System.Windows.Forms.Button();
+            this.btn_regretiroinsumo = new System.Windows.Forms.Button();
+            this.btn_ordencompra = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.btn_Mestadopreparacion = new System.Windows.Forms.Button();
+            this.btn_Mformapago = new System.Windows.Forms.Button();
+            this.btn_Mmotivoreclamo = new System.Windows.Forms.Button();
+            this.btn_Mtipoinsumos = new System.Windows.Forms.Button();
+            this.btn_Mproveedor = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // btn_Mproveedor
+            // button8
             // 
-            this.btn_Mproveedor.Location = new System.Drawing.Point(504, 69);
-            this.btn_Mproveedor.Name = "btn_Mproveedor";
-            this.btn_Mproveedor.Size = new System.Drawing.Size(151, 38);
-            this.btn_Mproveedor.TabIndex = 0;
-            this.btn_Mproveedor.Text = "Mantenedor Proveedor";
-            this.btn_Mproveedor.UseVisualStyleBackColor = true;
-            this.btn_Mproveedor.Click += new System.EventHandler(this.btn_Mproveedor_Click);
+            this.button8.Location = new System.Drawing.Point(549, 274);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(180, 52);
+            this.button8.TabIndex = 36;
+            this.button8.Text = "Realiza Pedidos / Ingreso Ventas";
+            this.button8.UseVisualStyleBackColor = true;
             // 
-            // btn_Mtipoinsumos
+            // btnregnoconformidadinsumos
             // 
-            this.btn_Mtipoinsumos.Location = new System.Drawing.Point(504, 128);
-            this.btn_Mtipoinsumos.Name = "btn_Mtipoinsumos";
-            this.btn_Mtipoinsumos.Size = new System.Drawing.Size(151, 38);
-            this.btn_Mtipoinsumos.TabIndex = 1;
-            this.btn_Mtipoinsumos.Text = "Mantenedor Tipo Insumos";
-            this.btn_Mtipoinsumos.UseVisualStyleBackColor = true;
-            this.btn_Mtipoinsumos.Click += new System.EventHandler(this.btn_Mtipoinsumos_Click);
+            this.btnregnoconformidadinsumos.Location = new System.Drawing.Point(308, 368);
+            this.btnregnoconformidadinsumos.Name = "btnregnoconformidadinsumos";
+            this.btnregnoconformidadinsumos.Size = new System.Drawing.Size(180, 51);
+            this.btnregnoconformidadinsumos.TabIndex = 35;
+            this.btnregnoconformidadinsumos.Text = "Registrar No Conformidad De Insumos";
+            this.btnregnoconformidadinsumos.UseVisualStyleBackColor = true;
             // 
-            // btn_Mmotivoreclamo
+            // btnrecepcioncomanda
             // 
-            this.btn_Mmotivoreclamo.Location = new System.Drawing.Point(504, 260);
-            this.btn_Mmotivoreclamo.Name = "btn_Mmotivoreclamo";
-            this.btn_Mmotivoreclamo.Size = new System.Drawing.Size(151, 38);
-            this.btn_Mmotivoreclamo.TabIndex = 2;
-            this.btn_Mmotivoreclamo.Text = "Mantenedor Motivo Reclamo";
-            this.btn_Mmotivoreclamo.UseVisualStyleBackColor = true;
-            this.btn_Mmotivoreclamo.Click += new System.EventHandler(this.btn_Mmotivoreclamo_Click);
+            this.btnrecepcioncomanda.Location = new System.Drawing.Point(308, 311);
+            this.btnrecepcioncomanda.Name = "btnrecepcioncomanda";
+            this.btnrecepcioncomanda.Size = new System.Drawing.Size(180, 51);
+            this.btnrecepcioncomanda.TabIndex = 34;
+            this.btnrecepcioncomanda.Text = "Recepcion Comanda";
+            this.btnrecepcioncomanda.UseVisualStyleBackColor = true;
             // 
-            // btn_Mformapago
+            // btnmonitoreopedidos
             // 
-            this.btn_Mformapago.Location = new System.Drawing.Point(504, 321);
-            this.btn_Mformapago.Name = "btn_Mformapago";
-            this.btn_Mformapago.Size = new System.Drawing.Size(151, 38);
-            this.btn_Mformapago.TabIndex = 3;
-            this.btn_Mformapago.Text = "Mantenedor Forma Pago";
-            this.btn_Mformapago.UseVisualStyleBackColor = true;
-            this.btn_Mformapago.Click += new System.EventHandler(this.btn_Mformapago_Click);
+            this.btnmonitoreopedidos.Location = new System.Drawing.Point(308, 253);
+            this.btnmonitoreopedidos.Name = "btnmonitoreopedidos";
+            this.btnmonitoreopedidos.Size = new System.Drawing.Size(180, 52);
+            this.btnmonitoreopedidos.TabIndex = 33;
+            this.btnmonitoreopedidos.Text = "Monitoreo Pedidos";
+            this.btnmonitoreopedidos.UseVisualStyleBackColor = true;
             // 
-            // btn_Mestadopreparacion
+            // btnfallamantenimiento
             // 
-            this.btn_Mestadopreparacion.Location = new System.Drawing.Point(504, 195);
-            this.btn_Mestadopreparacion.Name = "btn_Mestadopreparacion";
-            this.btn_Mestadopreparacion.Size = new System.Drawing.Size(151, 38);
-            this.btn_Mestadopreparacion.TabIndex = 4;
-            this.btn_Mestadopreparacion.Text = "Mantenedor Estado Preparación";
-            this.btn_Mestadopreparacion.UseVisualStyleBackColor = true;
-            this.btn_Mestadopreparacion.Click += new System.EventHandler(this.btn_Mestadopreparacion_Click);
+            this.btnfallamantenimiento.Location = new System.Drawing.Point(308, 195);
+            this.btnfallamantenimiento.Name = "btnfallamantenimiento";
+            this.btnfallamantenimiento.Size = new System.Drawing.Size(180, 52);
+            this.btnfallamantenimiento.TabIndex = 32;
+            this.btnfallamantenimiento.Text = "Falla Mantenimiento";
+            this.btnfallamantenimiento.UseVisualStyleBackColor = true;
+            // 
+            // btn_requirimientos
+            // 
+            this.btn_requirimientos.Location = new System.Drawing.Point(61, 244);
+            this.btn_requirimientos.Name = "btn_requirimientos";
+            this.btn_requirimientos.Size = new System.Drawing.Size(180, 51);
+            this.btn_requirimientos.TabIndex = 31;
+            this.btn_requirimientos.Text = "Realiza Requerimientos";
+            this.btn_requirimientos.UseVisualStyleBackColor = true;
+            // 
+            // btn_realizanotaingresoinsumo
+            // 
+            this.btn_realizanotaingresoinsumo.Location = new System.Drawing.Point(61, 417);
+            this.btn_realizanotaingresoinsumo.Name = "btn_realizanotaingresoinsumo";
+            this.btn_realizanotaingresoinsumo.Size = new System.Drawing.Size(180, 51);
+            this.btn_realizanotaingresoinsumo.TabIndex = 30;
+            this.btn_realizanotaingresoinsumo.Text = "Realiza Nota Ingreso Insumos";
+            this.btn_realizanotaingresoinsumo.UseVisualStyleBackColor = true;
+            // 
+            // btn_regretiroinsumo
+            // 
+            this.btn_regretiroinsumo.Location = new System.Drawing.Point(61, 359);
+            this.btn_regretiroinsumo.Name = "btn_regretiroinsumo";
+            this.btn_regretiroinsumo.Size = new System.Drawing.Size(180, 52);
+            this.btn_regretiroinsumo.TabIndex = 29;
+            this.btn_regretiroinsumo.Text = "Registrar Retiro Insumos";
+            this.btn_regretiroinsumo.UseVisualStyleBackColor = true;
+            // 
+            // btn_ordencompra
+            // 
+            this.btn_ordencompra.Location = new System.Drawing.Point(61, 301);
+            this.btn_ordencompra.Name = "btn_ordencompra";
+            this.btn_ordencompra.Size = new System.Drawing.Size(180, 52);
+            this.btn_ordencompra.TabIndex = 28;
+            this.btn_ordencompra.Text = "Orden Compra";
+            this.btn_ordencompra.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(570, 63);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 19);
+            this.label3.TabIndex = 27;
+            this.label3.Text = "AREA DE VENTAS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(85, 63);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 19);
+            this.label2.TabIndex = 26;
+            this.label2.Text = "AREA DE ALMACEN";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(521, 36);
+            this.label1.Location = new System.Drawing.Point(336, 63);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(117, 13);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Sección Mantenedores";
+            this.label1.Size = new System.Drawing.Size(128, 19);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "AREA DE COCINA";
+            // 
+            // btn_Mestadopreparacion
+            // 
+            this.btn_Mestadopreparacion.Location = new System.Drawing.Point(308, 110);
+            this.btn_Mestadopreparacion.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Mestadopreparacion.Name = "btn_Mestadopreparacion";
+            this.btn_Mestadopreparacion.Size = new System.Drawing.Size(180, 51);
+            this.btn_Mestadopreparacion.TabIndex = 24;
+            this.btn_Mestadopreparacion.Text = "Mantenedor Estado Preparación";
+            this.btn_Mestadopreparacion.UseVisualStyleBackColor = true;
+            // 
+            // btn_Mformapago
+            // 
+            this.btn_Mformapago.Location = new System.Drawing.Point(549, 172);
+            this.btn_Mformapago.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Mformapago.Name = "btn_Mformapago";
+            this.btn_Mformapago.Size = new System.Drawing.Size(180, 51);
+            this.btn_Mformapago.TabIndex = 23;
+            this.btn_Mformapago.Text = "Mantenedor Forma Pago";
+            this.btn_Mformapago.UseVisualStyleBackColor = true;
+            // 
+            // btn_Mmotivoreclamo
+            // 
+            this.btn_Mmotivoreclamo.Location = new System.Drawing.Point(549, 110);
+            this.btn_Mmotivoreclamo.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Mmotivoreclamo.Name = "btn_Mmotivoreclamo";
+            this.btn_Mmotivoreclamo.Size = new System.Drawing.Size(180, 51);
+            this.btn_Mmotivoreclamo.TabIndex = 22;
+            this.btn_Mmotivoreclamo.Text = "Mantenedor Motivo Reclamo";
+            this.btn_Mmotivoreclamo.UseVisualStyleBackColor = true;
+            // 
+            // btn_Mtipoinsumos
+            // 
+            this.btn_Mtipoinsumos.Location = new System.Drawing.Point(61, 163);
+            this.btn_Mtipoinsumos.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Mtipoinsumos.Name = "btn_Mtipoinsumos";
+            this.btn_Mtipoinsumos.Size = new System.Drawing.Size(180, 51);
+            this.btn_Mtipoinsumos.TabIndex = 21;
+            this.btn_Mtipoinsumos.Text = "Mantenedor Tipo Insumos";
+            this.btn_Mtipoinsumos.UseVisualStyleBackColor = true;
+            // 
+            // btn_Mproveedor
+            // 
+            this.btn_Mproveedor.Location = new System.Drawing.Point(61, 101);
+            this.btn_Mproveedor.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Mproveedor.Name = "btn_Mproveedor";
+            this.btn_Mproveedor.Size = new System.Drawing.Size(180, 51);
+            this.btn_Mproveedor.TabIndex = 20;
+            this.btn_Mproveedor.Text = "Mantenedor Proveedor";
+            this.btn_Mproveedor.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(810, 524);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.btnregnoconformidadinsumos);
+            this.Controls.Add(this.btnrecepcioncomanda);
+            this.Controls.Add(this.btnmonitoreopedidos);
+            this.Controls.Add(this.btnfallamantenimiento);
+            this.Controls.Add(this.btn_requirimientos);
+            this.Controls.Add(this.btn_realizanotaingresoinsumo);
+            this.Controls.Add(this.btn_regretiroinsumo);
+            this.Controls.Add(this.btn_ordencompra);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btn_Mestadopreparacion);
             this.Controls.Add(this.btn_Mformapago);
             this.Controls.Add(this.btn_Mmotivoreclamo);
             this.Controls.Add(this.btn_Mtipoinsumos);
             this.Controls.Add(this.btn_Mproveedor);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "MenúPrincipal";
             this.ResumeLayout(false);
@@ -115,12 +241,23 @@
 
         #endregion
 
-        private System.Windows.Forms.Button btn_Mproveedor;
-        private System.Windows.Forms.Button btn_Mtipoinsumos;
-        private System.Windows.Forms.Button btn_Mmotivoreclamo;
-        private System.Windows.Forms.Button btn_Mformapago;
-        private System.Windows.Forms.Button btn_Mestadopreparacion;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button btnregnoconformidadinsumos;
+        private System.Windows.Forms.Button btnrecepcioncomanda;
+        private System.Windows.Forms.Button btnmonitoreopedidos;
+        private System.Windows.Forms.Button btnfallamantenimiento;
+        private System.Windows.Forms.Button btn_requirimientos;
+        private System.Windows.Forms.Button btn_realizanotaingresoinsumo;
+        private System.Windows.Forms.Button btn_regretiroinsumo;
+        private System.Windows.Forms.Button btn_ordencompra;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_Mestadopreparacion;
+        private System.Windows.Forms.Button btn_Mformapago;
+        private System.Windows.Forms.Button btn_Mmotivoreclamo;
+        private System.Windows.Forms.Button btn_Mtipoinsumos;
+        private System.Windows.Forms.Button btn_Mproveedor;
     }
 }
 
